d5e16f349eccc29de8782e537f5637ca27da6631
lemwaizz <mwaidennis26@gmail.com>
2025-01-17 15:59:58 +0200

    bins=np.arange()

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+bins=np.arange(0,101,10)
+plt.hist(student_grades, bins=bins, color = "blue", edgecolor = "black")
+plt.xlabel("Grades")
+plt.ylabel("Number of Students")
+plt.title("Project A")
+plt.show()
