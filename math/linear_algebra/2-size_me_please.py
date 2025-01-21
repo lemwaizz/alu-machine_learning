0dd246c1a490ea772b91bd5745b04efd00a536ae
lemwaizz <mwaidennis26@gmail.com>
2025-01-21 15:38:57 +0200

    3-D matrices

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
-    if not matrix or not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise ValueError("Input must be a non-empty 2D list")
-    
-    rows = len(matrix)
-    columns = len(matrix[0])
-    
-    return [rows, columns]
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0] if matrix else[]
+    return shape
