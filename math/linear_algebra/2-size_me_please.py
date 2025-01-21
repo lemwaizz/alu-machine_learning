ab99f7ee2cfe5ea8207a5f33d6f0e7bf5f14d137
lemwaizz <mwaidennis26@gmail.com>
2025-01-21 15:46:33 +0200

    white space

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -4,10 +4,11 @@
 This module provides a function to determine the shape of a matrix.
 """
 
+
 def matrix_shape(matrix):
     """calculate shape of all dimensional matrices"""
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
-        matrix = matrix[0] if matrix else[]
+        matrix = matrix[0] if matrix else []
     return shape
