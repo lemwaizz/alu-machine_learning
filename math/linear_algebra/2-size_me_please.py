8bf576f9d9496383541b8896cb23ce44afe469ec
lemwaizz <mwaidennis26@gmail.com>
2025-01-21 15:22:05 +0200

    added error handling

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+
 def matrix_shape(matrix):
+    if not matrix or not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise ValueError("Input must be a non-empty 2D list")
+    
     rows = len(matrix)
     columns = len(matrix[0])
-    shape = []
-    shape.append(rows)
-    shape.append(columns)
-    return shape
+    
+    return [rows, columns]
