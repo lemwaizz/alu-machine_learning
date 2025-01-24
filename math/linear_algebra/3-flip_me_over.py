86ef6a1373b277e924ea35b4b885e2753a79cab4
lemwaizz <mwaidennis26@gmail.com>
2025-01-24 12:38:40 +0200

    pycodestyle

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -4,11 +4,13 @@
 This module provides a function that transposes a matrix
 """
 
+
 def matrix_transpose(matrix):
     """
     Returns the transpose of a matrix
     """
-    result_matrix = [[0 for _ in range(len(matrix))]for _ in range(len(matrix[0]))]
+    result_matrix = [[0 for _ in range(len(matrix))]
+                     for _ in range(len(matrix[0]))]
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             result_matrix[j][i] = matrix[i][j]
