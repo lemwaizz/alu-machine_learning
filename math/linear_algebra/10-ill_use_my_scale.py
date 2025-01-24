eeee4d7d7f109157273483103e7665482e19afcd
lemwaizz <mwaidennis26@gmail.com>
2025-01-24 20:30:08 +0200

    complete

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import numpy as np
+
+"""
+This module calculates the shape of a matrix
+"""
+
+
+def np_shape(matrix):
+    """
+    This function calculates shape of a matrix
+    """
+    return np.shape(matrix)
