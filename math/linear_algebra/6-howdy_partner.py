e396711de96c18f02d0fbdea66cb6adb03a5029c
lemwaizz <mwaidennis26@gmail.com>
2025-01-24 13:39:34 +0200

    documentation

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -6,6 +6,9 @@ This module concatenates two lists
 
 
 def cat_arrays(arr1, arr2):
+    """
+    This function concatenates two strings
+    """
     if not isinstance(arr1, list) or not isinstance(arr2, list):
         return None
     else:
