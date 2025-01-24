6d0019a284b285bb82d8c9c845df978e9a4b1a3c
lemwaizz <mwaidennis26@gmail.com>
2025-01-24 12:32:45 +0200

    [[0 for _ in range(len(mat))] for _ in range(len(mat(0))) ]

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+This module provides a function that transposes a matrix
+"""
+
+def matrix_transpose(matrix):
+    """
+    Returns the transpose of a matrix
+    """
+    result_matrix = [[0 for _ in range(len(matrix))]for _ in range(len(matrix[0]))]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            result_matrix[j][i] = matrix[i][j]
+    return result_matrix
