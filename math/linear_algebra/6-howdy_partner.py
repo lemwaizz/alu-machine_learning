444f769879c430a760ba342f68148e1f3824218a
lemwaizz <mwaidennis26@gmail.com>
2025-01-24 13:37:46 +0200

    concatenation

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+This module concatenates two lists
+"""
+
+
+def cat_arrays(arr1, arr2):
+    if not isinstance(arr1, list) or not isinstance(arr2, list):
+        return None
+    else:
+        ans = arr1 + arr2
+        return ans
