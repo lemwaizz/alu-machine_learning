54b270ba87f67d8690717c75500bbee1030505de
lemwaizz <mwaidennis26@gmail.com>
2025-01-24 13:06:02 +0200

    have to create for both 1d and 2d

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""
+This module adds two arrays (1D or 2D) together.
+"""
+
+
+def add_arrays(arr1, arr2):
+    """
+    This function checks array dimensionality then proceeds to add them.
+    
+    Args:
+        arr1 (list of int/float or list of list of int/float): The first array.
+        arr2 (list of int/float or list of list of int/float): The second array.
+    
+    Returns:
+        list of int/float or list of list of int/float: The resulting array after addition.
+    
+    Raises:
+        ValueError: If the arrays have incompatible dimensions.
+    """
+    if isinstance(arr1[0], list) and isinstance(arr2[0], list):
+        if len(arr1) != len(arr2) or len(arr1[0]) != len(arr2[0]):
+            raise ValueError("Incompatible matrix dimensions")
+        
+        result_matrix = [[0 for _ in range(len(arr1[0]))] for _ in range(len(arr1))]
+        
+        for i in range(len(arr1)):
+            for j in range(len(arr1[0])):
+                result_matrix[i][j] = arr1[i][j] + arr2[i][j]
+    else:
+        if len(arr1) != len(arr2):
+            raise ValueError("Incompatible array dimensions")
+        
+        result_matrix = [0 for _ in range(len(arr1))]
+        
+        for i in range(len(arr1)):
+            result_matrix[i] = arr1[i] + arr2[i]
+    
+    return result_matrix
